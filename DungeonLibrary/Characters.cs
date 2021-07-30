b9a5b0c38935515c9a8ec93e40f65d87ade76ccf
Amanda Allen <amandaallen0227@gmail.com>
2021-07-29 23:48:09 -0500

    Created Library + character class

---
+++ DungeonLibrary/Characters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Characters
+    {
+        public static void Header(string title)
+        {
+            Console.Title = "||||" + title + "||||";
+            Console.WriteLine(title);
+        } //end header
+    } //end class
+} //end namespace
